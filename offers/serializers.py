77a1d4bb7dde349e9f351052db1fc948e1f90466
FernandoMacarlupu <70768922+FernandoMacarlupu@users.noreply.github.com>
2023-05-11 15:55:33 -0500

    Modulo_Ofertas-Creacion_Serializers

---
+++ offers/serializers.py
@@ -0,0 +1,44 @@
+﻿from rest_framework import serializers
+from offers.models import CompetencyType, Competency, JobOffer, JobOfferxUser, OfferNotification, AreaxPositionxCompetency, EmployeexCompetency
+
+class CompetencyTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompetencyType
+        fields = '__all__'
+
+class CompetencySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Competency
+        fields = '__all__'
+
+class JobOfferSerializer(serializers.ModelSerializer):
+    #position = 
+    #area = 
+    #candidates = 
+    class Meta:
+        model = JobOffer
+        fields = '__all__'
+
+class JobOfferxUserSerializer(serializers.ModelSerializer):
+    #user = 
+    class Meta:
+        model = JobOfferxUser
+        fields = '__all__'
+
+class OfferNotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OfferNotification
+        fields = '__all__'
+
+class AreaxPositionxCompetencySerializer(serializers.ModelSerializer):
+    #position =
+    #area =
+    class Meta:
+        model = AreaxPositionxCompetency
+        fields = '__all__'
+
+class EmployeexCompetencySerializer(serializers.ModelSerializer):
+    #employee =
+    class Meta:
+        model = EmployeexCompetency
+        fields = '__all__'
