00c1afe43d52c63e7be61f12163193f65011dc55
FernandoMacarlupu <70768922+FernandoMacarlupu@users.noreply.github.com>
2023-05-24 22:09:08 -0500

    Api Crear Competencia 1.0

---
+++ gaps/urls.py
@@ -0,0 +1,10 @@
+﻿from django.urls import include, path
+from gaps.views import CompetenciaView
+
+brechas_patterns = [
+    path('competencias', CompetenciaView.as_view()),
+]
+
+urlpatterns = [
+    path('brechas/', include(brechas_patterns)),
+]
--- gaps/views.py
+++ gaps/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from rest_framework import status
+from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from zappa.asynchronous import task
+from gaps.models import Competencia, TipoCompetencia, CompetenciaXAreaXPosicion, CompetenciaXEmpleado, NecesidadCapacitacion
+from gaps.serializers import CompetenciaSerializer, TipoCompetenciaSerializer, CompetenciaXAreaXPosicionSerializer, CompetenciaXEmpleadoSerializer, NecesidadCapacitacionSerializer
 # Create your views here.
+
+class CompetenciaView(APIView):
+    def get(self, request):
+        competencias = Competencia.objects.all()
+        competencias_serializer = CompetenciaSerializer(competencias,many = True)
+        return Response(competencias_serializer.data, status = status.HTTP_200_OK)
+    
+    def post(self, request):
+        competencias_serializer = CompetenciaSerializer(data = request.data, context = request.data)
+        if competencias_serializer.is_valid():
+            competencias_serializer.save()
+            return Response(competencias_serializer.data,status=status.HTTP_200_OK)
+        return Response(None,status=status.HTTP_400_BAD_REQUEST)
