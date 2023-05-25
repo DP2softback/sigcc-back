4ec6981f4863e9b61c1c604dc16f43fe76404fb8
FernandoMacarlupu <70768922+FernandoMacarlupu@users.noreply.github.com>
2023-05-24 19:38:00 -0500

    Modificacion de models Competencia

--- gaps/models.py
+++ gaps/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 from evaluations_and_promotions.models import Area, Position
 from login.models import Employee
+
 # Create your models here.
+
+class TipoCompetencia(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    abreviatura = models.CharField(max_length=30, blank=True, null=True)
+    nombre = models.CharField(max_length=100, blank=True, null=True)
+    descripcion = models.CharField(max_length=300, blank=True, null=True)
+    activo = models.BooleanField(default=True)
 
 class Competencia(models.Model):
     id = models.BigAutoField(primary_key = True)
     codigo = models.CharField(max_length=12, blank=True, null=True)
     nombre = models.CharField(max_length=100, blank=True, null=True)
-    descripcion = models.CharField(max_length=200, blank=True, null=True)
+    descripcion = models.CharField(max_length=300, blank=True, null=True)
     activo = models.BooleanField(default=True)
-    #tipo = models.ForeignKey(TipoCompetencia, on_delete=models.CASCADE, null=True, blank=True)
+    tipo = models.ForeignKey(TipoCompetencia, on_delete=models.CASCADE, null=True, blank=True)
 	
 class CompetenciaXAreaXPosicion(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -24,8 +32,12 @@ class CompetenciaXEmpleado(models.Model):
     competencia = models.ForeignKey(Competencia, on_delete=models.CASCADE, null=True, blank=True)
     empleado = models.ForeignKey(Employee, on_delete=models.CASCADE, null=True, blank=True)
     nivelActual = models.IntegerField(blank=True,null =True)
+    nivelRequerido = models.IntegerField(blank=True,null =True)
+    nivelBrecha = models.IntegerField(blank=True,null =True)
+    adecuacion = models.FloatField(blank=True,null =True)
     tieneCertificado = models.BooleanField(default=False)
     agregadoPorEmpleado = models.BooleanField(default=False)
+    requeridoParaPuesto = models.BooleanField(default=False)
     activo = models.BooleanField(default=True)
 
 class NecesidadCapacitacion(models.Model):
@@ -36,12 +48,7 @@ class NecesidadCapacitacion(models.Model):
     estado = models.IntegerField(blank=True,null =True)  #0: por solucionar, 1: en proceso, 2: solucionado 
     nivelActual = models.IntegerField(blank=True,null =True)
     nivelRequerido = models.IntegerField(blank=True,null =True)
-    brecha = models.IntegerField(blank=True,null =True) #1: brecha de nivel 1, 2: brecha de nivel 2, 3: brecha de nivel 3, 4: brecha de nivel 4
+    nivelBrecha = models.IntegerField(blank=True,null =True) #1: brecha de nivel 1, 2: brecha de nivel 2, 3: brecha de nivel 3, 4: brecha de nivel 4
     activo = models.BooleanField(default=True)
 
-class TipoCompetencia(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    abreviatura = models.CharField(max_length=30, blank=True, null=True)
-    nombre = models.CharField(max_length=100, blank=True, null=True)
-    descripcion = models.CharField(max_length=300, blank=True, null=True)
-    activo = models.BooleanField(default=True)
+
