2e7de521c124d7d90ec2a071569e3ef0b81a07c6
FernandoMacarlupu <70768922+FernandoMacarlupu@users.noreply.github.com>
2023-05-26 19:01:49 -0500

    Desactivar Competencia 1.0 - Crear 2.0

--- gaps/views.py
+++ gaps/views.py
@@ -11,22 +11,44 @@ from gaps.serializers import CompetenciaSerializer, TipoCompetenciaSerializer, C
 # Create your views here.
 
 class CompetenciaView(APIView):
-    def get(self, request):
+    def get(self, request,id=0):
         competencias = Competencia.objects.all()
         competencias_serializer = CompetenciaSerializer(competencias,many = True)
         return Response(competencias_serializer.data, status = status.HTTP_200_OK)
     
-    def post(self, request):
+    def post(self, request,id=0):
         competencias_serializer = CompetenciaSerializer(data = request.data, context = request.data)
+        if competencias_serializer.is_valid():
+            competencias_serializer.save()
+            idTipoComp = request.data["tipo"]
+            tipoCompetencia = TipoCompetencia.objects.filter(id=idTipoComp).values()
+            competencia = Competencia.objects.filter(id=competencias_serializer.data['id']).first()
+            campos = {'codigo': tipoCompetencia[0]['abreviatura'] + str(competencias_serializer.data['id']).zfill(9)}
+            competencias_serializer2 = CompetenciaSerializer(competencia, data = campos)
+            if competencias_serializer2.is_valid():
+                competencias_serializer2.save()
+                return Response(competencias_serializer2.data,status=status.HTTP_200_OK)
+            return Response(competencias_serializer.data,status=status.HTTP_200_OK)
+        return Response(None,status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request,id=0):
+        idComp = request.data["id"]
+        if request.data["tipo"] is not None:
+            idTipoComp = request.data["tipo"]
+            tipoCompetencia = TipoCompetencia.objects.filter(id=idTipoComp).values()
+            campos = {'codigo': tipoCompetencia[0]['abreviatura'] + str(request.data["id"]).zfill(9)}
+            request.data["codigo"] = campos['codigo']
+        competencia = Competencia.objects.filter(id=idComp).first()
+        competencias_serializer = CompetenciaSerializer(competencia, data = request.data, context = request.data)
         if competencias_serializer.is_valid():
             competencias_serializer.save()
             return Response(competencias_serializer.data,status=status.HTTP_200_OK)
         return Response(None,status=status.HTTP_400_BAD_REQUEST)
     
-    def put(self, request):
-        idComp = request.PUT.get('id')
-        competencia = Competencia.objects.filter(id=idComp).first()
-        competencias_serializer = CompetenciaSerializer(competencia, data = request.data, context = request.data)
+    def delete(self, request, id=0):
+        competencia = Competencia.objects.filter(id=id).first()
+        campos = {'activo': 'false'}
+        competencias_serializer = CompetenciaSerializer(competencia, data = campos)
         if competencias_serializer.is_valid():
             competencias_serializer.save()
             return Response(competencias_serializer.data,status=status.HTTP_200_OK)
