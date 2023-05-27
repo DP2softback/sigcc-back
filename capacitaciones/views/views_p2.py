0fc777493199ffc87ccdec34209ba4ce7ba194f9
gianella123 <70769374+gianella123@users.noreply.github.com>
2023-05-26 23:35:23 -0500

    Modificacion Json

--- capacitaciones/views/views_p2.py
+++ capacitaciones/views/views_p2.py
@@ -75,42 +75,6 @@ class CursoEmpresaDetailAPIView(APIView):
         return Response({"message": "Curso eliminado"}, status=status.HTTP_200_OK)
 
 
-class SesionAPIView(APIView):
-    permission_classes = [AllowAny]
-    @transaction.atomic
-    def dispatch(self, request, *args, **kwargs):
-        return super().dispatch(request, *args, **kwargs)
-    
-    def get(self, request):
-        sesiones_emp = Sesion.objects.all()
-        sesiones_emp_serializer = SesionSerializer(sesiones_emp, many=True)
-        return Response(sesiones_emp_serializer.data, status = status.HTTP_200_OK)
-    
-    def post(self, request):
-
-        sesiones_emp_serializer = SesionSerializer(data = request.data, context = request.data)
-
-        if sesiones_emp_serializer.is_valid():
-            curso_empresa_id= request.data['curso_empresa_id']
-            sesiones_emp = sesiones_emp_serializer.save(cursoEmpresa_id=curso_empresa_id)
-
-            for tema_sesion in request.data['temas']:
-                print(tema_sesion)
-                tema_serializer = TemaSerializer(data=tema_sesion)
-
-                if tema_serializer.is_valid():
-                    tema_serializer.validated_data['sesion']= sesiones_emp
-                    tema = Tema.objects.filter(nombre=tema_serializer.validated_data['nombre']).first()
-                    if tema is None:
-                        tema = tema_serializer.save()
-                else:
-                    return Response({"message": "No se pudo crear el tema {}".format(tema_sesion['nombre'])}, status=status.HTTP_400_BAD_REQUEST)
-
-            return Response({'id': sesiones_emp.id,
-                            'message': 'La sesion se ha con sus temas creado correctamente'}, status=status.HTTP_200_OK)
-
-        return Response(sesiones_emp_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 class SesionDetailAPIView(APIView):
     permission_classes = [AllowAny]
     @transaction.atomic
