4cefb3c86511a4a717eb2aebf049309a43839239
FernandoMacarlupu <70768922+FernandoMacarlupu@users.noreply.github.com>
2023-05-30 23:08:53 -0500

    Cambios app gaps - Cambio de zappa_settings y Dp2softback/settings.py (tokens)

--- gaps/models.py
+++ gaps/models.py
@@ -4,59 +4,60 @@ from login.models import Employee
 
 # Create your models here.
 
-class TipoCompetencia(models.Model):
+class CompetenceType(models.Model):
     id = models.BigAutoField(primary_key=True)
-    abreviatura = models.CharField(max_length=30, blank=True, null=True)
-    nombre = models.CharField(max_length=100, blank=True, null=True)
-    descripcion = models.CharField(max_length=300, blank=True, null=True)
-    activo = models.BooleanField(default=True)
+    abbreviation = models.CharField(max_length=30, blank=True, null=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    description = models.CharField(max_length=300, blank=True, null=True)
+    active = models.BooleanField(default=True)
+    upperType = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
     export_fields = [
         'id',
-        'abreviatura',
-        'nombre',
-        'descripcion',
-        'activo'
+        'abbreviation',
+        'name',
+        'description',
+        'active'
     ]
     
-class Competencia(models.Model):
+class Competence(models.Model):
     id = models.BigAutoField(primary_key = True)
-    codigo = models.CharField(max_length=12, blank=True, null=True)
-    nombre = models.CharField(max_length=100, blank=True, null=True)
-    descripcion = models.CharField(max_length=300, blank=True, null=True)
-    activo = models.BooleanField(default=True)
-    tipo = models.ForeignKey(TipoCompetencia, on_delete=models.CASCADE, null=True, blank=True)
+    code = models.CharField(max_length=12, blank=True, null=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    description = models.CharField(max_length=300, blank=True, null=True)
+    active = models.BooleanField(default=True)
+    type = models.ForeignKey(CompetenceType, on_delete=models.CASCADE, null=True, blank=True)
 	
-class CompetenciaXAreaXPosicion(models.Model):
+class CompetenceXAreaXPosition(models.Model):
     id = models.BigAutoField(primary_key=True)
-    competencia = models.ForeignKey(Competencia, on_delete=models.CASCADE, null=True, blank=True)
-    posicion = models.ForeignKey(Position, on_delete=models.CASCADE, null=True, blank=True)
+    competence = models.ForeignKey(Competence, on_delete=models.CASCADE, null=True, blank=True)
+    position = models.ForeignKey(Position, on_delete=models.CASCADE, null=True, blank=True)
     area = models.ForeignKey(Area, on_delete=models.CASCADE, null=True, blank=True)
-    nivelRequerido = models.IntegerField(blank=True,null =True)
-    activo = models.BooleanField(default=True)
+    levelRequired = models.IntegerField(blank=True,null =True)
+    active = models.BooleanField(default=True)
 
-class CompetenciaXEmpleado(models.Model):
+class CompetenceXEmployee(models.Model):
     id = models.BigAutoField(primary_key=True)
-    competencia = models.ForeignKey(Competencia, on_delete=models.CASCADE, null=True, blank=True)
-    empleado = models.ForeignKey(Employee, on_delete=models.CASCADE, null=True, blank=True)
-    nivelActual = models.IntegerField(blank=True,null =True)
-    nivelRequerido = models.IntegerField(blank=True,null =True)
-    nivelBrecha = models.IntegerField(blank=True,null =True)
-    adecuacion = models.FloatField(blank=True,null =True)
-    tieneCertificado = models.BooleanField(default=False)
-    agregadoPorEmpleado = models.BooleanField(default=False)
-    requeridoParaPuesto = models.BooleanField(default=False)
-    activo = models.BooleanField(default=True)
+    competence = models.ForeignKey(Competence, on_delete=models.CASCADE, null=True, blank=True)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, null=True, blank=True)
+    levelCurrent = models.IntegerField(blank=True,null =True)
+    levelRequired = models.IntegerField(blank=True,null =True)
+    levelGap = models.IntegerField(blank=True,null =True)
+    likeness = models.FloatField(blank=True,null =True)
+    hasCertificate = models.BooleanField(default=False)
+    registerByEmployee = models.BooleanField(default=False)
+    requiredForPosition = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
 
-class NecesidadCapacitacion(models.Model):
+class TrainingNeed(models.Model):
     id = models.BigAutoField(primary_key=True)
-    competencia = models.ForeignKey(Competencia, on_delete=models.CASCADE, null=True, blank=True)
-    empleado = models.ForeignKey(Employee, on_delete=models.CASCADE, null=True, blank=True)
-    descripcion = models.CharField(max_length=200, blank=True, null=True)
-    estado = models.IntegerField(blank=True,null =True)  #1: por solucionar, 2: en proceso, 3: solucionado 
-    nivelActual = models.IntegerField(blank=True,null =True)
-    nivelRequerido = models.IntegerField(blank=True,null =True)
-    nivelBrecha = models.IntegerField(blank=True,null =True) #1: brecha de nivel 1, 2: brecha de nivel 2, 3: brecha de nivel 3, 4: brecha de nivel 4
-    tipo = models.IntegerField(blank=True,null =True) #1: de incorporacion, 2: de evaluacion 2, 3: de ascenso
-    activo = models.BooleanField(default=True)
+    competence = models.ForeignKey(Competence, on_delete=models.CASCADE, null=True, blank=True)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, null=True, blank=True)
+    description = models.CharField(max_length=200, blank=True, null=True)
+    state = models.IntegerField(blank=True,null =True)  #1: por solucionar, 2: en proceso, 3: solucionado 
+    levelCurrent = models.IntegerField(blank=True,null =True)
+    levelRequired = models.IntegerField(blank=True,null =True)
+    levelGap = models.IntegerField(blank=True,null =True) #1: brecha de nivel 1, 2: brecha de nivel 2, 3: brecha de nivel 3, 4: brecha de nivel 4
+    type = models.IntegerField(blank=True,null =True) #1: de incorporacion, 2: de evaluacion 2, 3: de ascenso
+    active = models.BooleanField(default=True)
 
 
