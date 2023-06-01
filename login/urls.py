7803885478bddbc2272c197f8332fce67930b9f4
Marco Bossio <marco.bossio@tuxpas.com>
2023-06-01 11:52:18 -0500

    added login options

--- login/urls.py
+++ login/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('login', LoginView.as_view()),
     path('whoiam', WhoIAmView.as_view()),
     path('logout', Logout.as_view()),
+    path('password_recovery', PasswordRecovery.as_view()),
+    path('password_recovery_code_check', PasswordRecoveryCodeCheck.as_view()),
+    path('password_change_without_login', PasswordChangeWithoutLogin.as_view()),
+    path('password_change_with_login', PasswordChangeWithLogin.as_view()),
+    
 ]
