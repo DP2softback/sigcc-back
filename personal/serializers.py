31a4395027819c13c73c0b5e0850f645ffd4a653
ClaudioLoz <a20180472@pucp.edu.pe>
2023-06-01 17:31:29 -0500

    cleanup

--- personal/serializers.py
+++ personal/serializers.py
@@ -26,45 +26,6 @@ class DynamicFieldsModelSerializer(serializers.ModelSerializer):
             for field_name in existing - allowed:
                 self.fields.pop(field_name)
 
-
-class UserSerializerRead(DynamicFieldsModelSerializer, serializers.ModelSerializer):
-    class Meta:
-        model = User
-        depth = 1
-        fields = ['id', 'created', 'modified', 'is_active', 'username', 'first_name', 'second_name', 'last_name', 'maiden_name', 'email', 'password', 'role_id']
-
-
-class UserSerializerWrite(DynamicFieldsModelSerializer, serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'created', 'modified', 'is_active', 'username', 'first_name', 'second_name', 'last_name', 'maiden_name', 'email', 'password', 'role_id']
-
-
-class EmployeeSerializerRead(DynamicFieldsModelSerializer, serializers.ModelSerializer):
-    class Meta:
-        model = Employee
-        depth = 1
-        fields = '__all__'
-
-
-class EmployeeSerializerWrite(DynamicFieldsModelSerializer, serializers.ModelSerializer):
-    class Meta:
-        model = Employee
-        fields = '__all__'
-
-
-class RoleSerializerRead(DynamicFieldsModelSerializer, serializers.ModelSerializer):
-    class Meta:
-        model = Role
-        depth = 1
-        fields = '__all__'
-
-
-class RoleSerializerWrite(DynamicFieldsModelSerializer, serializers.ModelSerializer):
-    class Meta:
-        model = Role
-        fields = '__all__'
-
 class HiringProcessSerializer(serializers.ModelSerializer):
     class Meta:
         model = HiringProcess
