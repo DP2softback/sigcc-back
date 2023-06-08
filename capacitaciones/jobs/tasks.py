b1e4465151c35c83f217e538bfb75a90bf2a4436
Alonso <a20170809@pucp.edu.pe>
2023-06-08 17:11:39 -0500

    asd

--- capacitaciones/jobs/tasks.py
+++ capacitaciones/jobs/tasks.py
@@ -60,7 +60,7 @@ def upload_new_course_in_queue(curso):
             'id_course': curso.pk
         }
 
-        if not os.path.exists(os.getenv('PATH_FILE_QUEUE')) or os.path.getsize(os.getenv('PATH_FILE_QUEUE')) == 0:
+        if not os.path.exists(os.getenv('PATH_FILE_QUEUE')):
             courses_data = []
         else:
             with open(os.getenv('PATH_FILE_QUEUE'), 'r+') as file:
--- capacitaciones/jobs/updater.py
+++ capacitaciones/jobs/updater.py
@@ -6,6 +6,6 @@ from capacitaciones.jobs.tasks import task_create_udemy_evaluation
 
 
 def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(task_create_udemy_evaluation, 'interval', seconds=60, start_date=datetime.now())
+    scheduler = BackgroundScheduler(start_delay=0)
+    scheduler.add_job(task_create_udemy_evaluation, 'interval', seconds=60)
     scheduler.start()
