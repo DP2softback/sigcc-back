d3611c09a8ab955406ae56380e90dd44c05206c8
Alonso <a20170809@pucp.edu.pe>
2023-06-08 16:53:43 -0500

    Update updater.py

--- capacitaciones/jobs/updater.py
+++ capacitaciones/jobs/updater.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from capacitaciones.jobs.tasks import task_create_udemy_evaluation
@@ -5,5 +7,5 @@ from capacitaciones.jobs.tasks import task_create_udemy_evaluation
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(task_create_udemy_evaluation, 'interval', seconds=60)
+    scheduler.add_job(task_create_udemy_evaluation, 'interval', seconds=60, start_date=datetime.now())
     scheduler.start()
