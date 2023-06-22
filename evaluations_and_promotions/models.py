109b9c7357d099e037f3a4ac95ca0f5f0cbd090e
Sebastian Portal <sebastian.portal@pucp.edu.pe>
2023-06-21 20:41:39 -0500

    New competence integration :^)

--- evaluations_and_promotions/models.py
+++ evaluations_and_promotions/models.py
@@ -90,19 +90,18 @@ class SubCategory(models.Model):
     name = models.TextField(blank=True, default='')
     description = models.TextField(blank=True, default='')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-
     class Meta:
         db_table = 'Competence'
 
-# class CompetencessXEmployee(models.Model):
-#     id = models.BigAutoField(primary_key=True)
-#     creationDate = models.DateTimeField(auto_now_add=True)
-#     modifiedDate = models.DateTimeField(auto_now=True)
-#     isActive = models.BooleanField(default=True)
-#     employee = models.ForeignKey(Employee, on_delete=models.CASCADE, null=True, blank=True)
-#     competence = models.ForeignKey(SubCategory, on_delete=models.CASCADE, null=True, blank=True)
-#     level = models.IntegerField(blank=True,null =True)
-#     score = models.FloatField(blank=True,null =True)  
+class CompetencessXEmployee(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    creationDate = models.DateTimeField(auto_now_add=True)
+    modifiedDate = models.DateTimeField(auto_now=True)
+    isActive = models.BooleanField(default=True)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, null=True, blank=True)
+    competence = models.ForeignKey(SubCategory, on_delete=models.CASCADE, null=True, blank=True)
+    level = models.TextField(blank=True,null =True)
+    score = models.FloatField(blank=True,null =True)  
 
 
 class EvaluationxSubCategory(models.Model):
